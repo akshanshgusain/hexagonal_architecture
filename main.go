bddb97670e0ee75db2af50cf5c8cd002cb4e47f2
akshanshgusain <akshanshgusain@gmail.com>
2022-11-22 13:01:29 +0530

    feat: added json encoder

--- main.go
+++ main.go
@@ -1,23 +1,45 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
 
-func main() {
-	// handlers
-	http.HandleFunc("/greet", greet)
+type Customers struct {
+	Name    string
+	City    string
+	Zipcode string
+}
 
+func main() {
+	// routes
+	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/getAllCustomers", getAllCustomers)
+
+	// Starting Server
 	err := http.ListenAndServe("localhost:8080", nil)
 	if err != nil {
-		return
+		log.Fatal(err.Error())
 	}
 }
+
+// Handlers
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	_, err := fmt.Fprint(w, "Hello Welcome to the Hexagonal Architecture APP")
 	if err != nil {
 		return
 	}
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customers{
+		{"Akshansh Gusain", "New Delhi", "110001"},
+		{"Priyanka Khurana", "New Delhi", "110001"},
+		{"Rachit Kawar", "Jodhpur", "132901"},
+	}
+
+	json.NewEncoder(w).Encode(customers)
 }
