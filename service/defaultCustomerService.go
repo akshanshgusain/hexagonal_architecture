eb8a32f1df81d74a745253df617530e8b3c86ea6
akshanshgusain <akshanshgusain@gmail.com>
2022-11-23 11:34:40 +0530

    feat(service): added getcustomer method

--- service/defaultCustomerService.go
+++ service/defaultCustomerService.go
@@ -12,6 +12,12 @@ func (s *DefaultCustomerService) GetAllCustomers() ([]domain.Customer, error) {
 	return s.repo.FindAll()
 }
 
+func (s *DefaultCustomerService) GetCustomer(id string) (*domain.Customer, error) {
+	return s.repo.ById(id)
+}
+
+// Helpers
+
 func NewCustomerService(repository domain.CustomerRepository) CustomerService {
 	return &DefaultCustomerService{repository}
 }
