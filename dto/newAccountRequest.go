c2216d3aa94311bc98f970a2dbf5b3aed4695ff2
akshanshgusain <akshanshgusain@gmail.com>
2022-11-24 15:15:19 +0530

    feat(dto): completed newaccountrequest and newaccountresponse

--- dto/newAccountRequest.go
+++ dto/newAccountRequest.go
@@ -1,4 +1,7 @@
 package dto
 
 type NewAccountRequest struct {
+	CustomerId  string  `json:"customer_id"`
+	AccountType string  `json:"account_type"`
+	Amount      float64 `json:"amount"`
 }
--- dto/newAccountResponse.go
+++ dto/newAccountResponse.go
@@ -1,4 +1,8 @@
 package dto
 
+// the api should return the new account id, when the new account is opened with the status
+// code as 201
+
 type NewAccountResponse struct {
+	AccountId string `json:"account_id"`
 }
